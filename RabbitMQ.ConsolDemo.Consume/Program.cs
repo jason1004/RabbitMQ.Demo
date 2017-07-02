4e4419953a1bd1ff812726ece5efccf2af4a8e9d
jason.mao <mjq1004@163.com>
2017-07-02 12:36:30 +0800

    添加项目文件。

---
+++ RabbitMQ.ConsolDemo.Consume/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace RabbitMQ.ConsolDemo.Consume
+{
+    class Program
+    {
+        private static readonly ConnectionFactory RabbitMqFactory = new ConnectionFactory
+        {
+            HostName = "www.c-code.xin",
+            UserName = "test",
+            Password = "pwd@rabbit.com",
+            VirtualHost = "/"
+        };
+        const string ExchangeName = "hello.exchange";
+        const string QueueName = "hello.queue";
+        private static string _routingKey = "hello";
+        static void Main(string[] args)
+        {
+            Console.Title = "消费者";
+            //Console.WriteLine("请输入RoutingKey：");
+            //var key = Console.ReadLine();
+            //_routingKey = key;
+
+            // 1.连接到RabbitmQ
+            IConnection conn = RabbitMqFactory.CreateConnection();
+            // 2.获取信道
+            IModel channel = conn.CreateModel();
+            // 3.声明交换器
+            channel.ExchangeDeclare(exchange: ExchangeName, type: "direct", durable: true, autoDelete: false, arguments: null);
+            // 4.申明队列
+            channel.QueueDeclare(queue: QueueName, durable: true, exclusive: false, autoDelete: false, arguments: null);
+            // 5.通过键“hello”将队列和交换器绑定起来
+            channel.QueueBind(queue: QueueName, exchange: ExchangeName, routingKey: _routingKey);
+
+            Console.WriteLine("请选择消息获取方式；推:1,拉:2");
+            var type = Console.ReadLine();
+
+            //6.消费
+            switch (type)
+            {
+                case "1":
+                    
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (ch, ea) =>
+                    {
+                        var body = ea.Body;
+                        Console.WriteLine();
+                        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " 收到RabbitMQ的消息:");
+                        Console.WriteLine(System.Text.Encoding.UTF8.GetString(body));
+
+                        //通过AMQP的basic.ack命令显示地向RabbitMQ发送一个确认，服务器会删除这条消息；
+                        //或者在订阅队列的时候就将auto_ack参数设置为true，消费者一旦接收到消息后，RabbitMQ视为其确认了消息；
+                        channel.BasicAck(ea.DeliveryTag, false); 
+                    };
+                    //命令订阅，信道将会被设置为接收模式；直到取消对队列的订阅为止
+                    var consumerTag = channel.BasicConsume(QueueName, false, consumer); 
+
+
+                    break;
+                case "2":
+                    while (true)
+                    {
+                        //向队列请求单条消息，
+                        //这里要注意：不应该将BasicGet放在一个循环里面代替BasicConsume，这样会影响性能。
+                        //BasicGet 会订阅->获取单条消息->取消订阅；
+                        var basicGet = channel.BasicGet(QueueName, false);
+                        if (basicGet==null)
+                        {
+                            Console.WriteLine("没有获取到消息！！！");
+                            Console.ReadKey();
+                            continue;
+                        }
+                        //通过AMQP的basic.ack命令显示地向RabbitMQ发送一个确认，服务器会删除这条消息；
+                        //或者在订阅队列的时候就将auto_ack参数设置为true，消费者一旦接收到消息后，RabbitMQ视为其确认了消息；
+                        channel.BasicAck(basicGet.DeliveryTag,false);  
+                        Console.WriteLine(DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " 收到RabbitMQ的消息:");
+                        Console.WriteLine(System.Text.Encoding.UTF8.GetString(basicGet.Body));
+                        Console.ReadKey();
+                    }
+
+            }
+            Console.ReadKey();
+        }
+
+    }
+}
---
+++ RabbitMQ.ConsolDemo/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using RabbitMQ.Client;
+
+namespace RabbitMQ.ConsolDemo
+{
+    class Program
+    {
+        private static readonly ConnectionFactory RabbitMqFactory = new ConnectionFactory
+        {
+            HostName = "www.c-code.xin",
+            UserName = "test",
+            Password = "pwd@rabbit.com",
+            VirtualHost = "/", //默认值： /
+            Port = 5672   //默认值：5672
+        };
+        const string ExchangeName = "hello.exchange";
+        //const string QueueName = "hello.queue";
+        const string RoutingKey = "hello";
+        static void Main(string[] args)
+        {
+            Console.Title = "产生者";
+            //1.连接到RabbitMQ
+            IConnection conn = RabbitMqFactory.CreateConnection();
+            //2.获取信道
+            IModel channel = conn.CreateModel();
+            //3.声明交换器
+            channel.ExchangeDeclare(exchange: ExchangeName, type: "direct", durable: true, autoDelete: false, arguments: null);
+
+            var str = string.Empty;
+            while (string.IsNullOrEmpty(str))
+            {
+                Console.WriteLine("请输入需要发送的消息：");
+                str = Console.ReadLine();
+
+                //4.创建消息内容
+                byte[] messageBodyBytes = System.Text.Encoding.UTF8.GetBytes("     " + DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff") + " : " + str);
+                //5.发布消息到指定交换器
+                channel.BasicPublish(ExchangeName,
+                    RoutingKey,
+                    null,    // new BasicProperties { Expiration = "3600000"} //可以指定失效时间
+                    messageBodyBytes);
+
+                str = string.Empty;
+            }
+
+            //关闭信道
+            channel.Close(); //channel.Dispose(); 也可以
+            //关闭连接
+            conn.Close();  //conn.Dispose(); 也可以
+        }
+
+        
+
+    }
+}

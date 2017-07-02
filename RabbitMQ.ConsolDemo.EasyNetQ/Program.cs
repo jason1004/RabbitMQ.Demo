5a011b2591b42cec384a37edda64dde2e96ba643
jason.mao <mjq1004@163.com>
2017-07-02 16:57:23 +0800

    EasyNetQ 使用

---
+++ RabbitMQ.ConsolDemo.EasyNetQ/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EasyNetQ;
+using EasyNetQ.Topology;
+
+namespace RabbitMQ.ConsolDemo.EasyNetQ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //https://github.com/EasyNetQ/EasyNetQ
+            //http://mikehadlow.blogspot.sg/2014/02/easynetq-layered-api.html
+
+            var bus = RabbitHutch.CreateBus("host=www.c-code.xin:5672;" +
+                                            "virtualHost=/;" +
+                                            "username=test;" +
+                                            "password=pwd@rabbit.com");
+
+            var msg = "";
+            while (string.IsNullOrEmpty(msg))
+            {
+                //  msg = Console.ReadLine();
+                msg = DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss.fff");
+                bus.Advanced.Publish(new Exchange("hello.exchange"), "hello",
+                    false, new Message<string>(msg));
+                msg =String.Empty;
+            }
+         
+            Console.ReadKey();
+        }
+
+
+
+    }
+
+    internal class MyMessage
+    {
+        public string Text { get; set; }
+    }
+}
